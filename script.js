57a1f27fddf8dbb6e4618ae8dfa564fd74872a82
ramedwararyan <143419537+ramedwararyan@users.noreply.github.com>
2025-07-25 14:43:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const makeChange = (c) => {
   // your name here
+	let change = {};
+
+  change.q = Math.floor(amount / 25);
+  amount = amount % 25;
+
+  change.d = Math.floor(amount / 10);
+  amount = amount % 10;
+
+  change.n = Math.floor(amount / 5);
+  amount = amount % 5;
+
+  change.p = amount; 
+
+	
+
+  return change;
 };
 
 // Do not the change the code below
